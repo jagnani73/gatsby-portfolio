63c57d4e1dd8a6c3f6070d1b74f1a726aaf6b38d
Yashvardhan Jagnani <jagnani73@users.noreply.github.com>
2020-07-05 00:40:55 +0530

    jobs

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,11 +1,72 @@
-import React from "react"
-import Title from "./Title"
+import React, { useState } from "react"
+import { graphql, useStaticQuery, Link } from "gatsby"
 import { FaAngleDoubleRight } from "react-icons/fa"
-import { graphql, useStaticQuery } from "gatsby"
-import { Link } from "gatsby"
+
+import Title from "./Title"
+
+const query = graphql`
+  {
+    allStrapiJobs(sort: { fields: strapiId, order: DESC }) {
+      nodes {
+        strapiId
+        company
+        date
+        position
+        desc {
+          id
+          name
+        }
+      }
+    }
+  }
+`
 
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  const data = useStaticQuery(query)
+
+  const [value, setValue] = useState(0)
+
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data
+
+  const { company, position, date, desc } = jobs[value]
+  console.log(company, position, date, desc)
+
+  return (
+    <section className="section jobs">
+      <Title title="experience" />
+      <div className="jobs-center">
+        <div className="btn-container">
+          {jobs.map((job, index) => (
+            <button
+              key={job.strapiId}
+              className={`job-btn ${index === value && " active"}`}
+              onClick={() => {
+                setValue(index)
+              }}
+            >
+              {job.company}
+            </button>
+          ))}
+        </div>
+        <article className="job-info">
+          <h3>{position}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{date}</p>
+          {desc.map(item => (
+            <div key={item.id} className="job-desc">
+              <FaAngleDoubleRight className="job-icon" />
+              <p>{item.name}</p>
+            </div>
+          ))}
+        </article>
+      </div>
+      <Link to="/about" className="btn center-btn">
+        More Info
+      </Link>
+    </section>
+  )
 }
 
 export default Jobs

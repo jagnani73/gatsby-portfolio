4a02007f9a3ae46b94c6da472ff0e1a61d3e42c6
Yashvardhan Jagnani <jagnani73@users.noreply.github.com>
2020-07-05 11:45:13 +0530

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,22 @@
 import React from "react"
-import Layout from "../components/Layout"
 import { Link } from "gatsby"
+
+import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 
 const Error = () => {
-  return <h2>error page</h2>
+  return (
+    <Layout>
+      <main className="error-page">
+        <div className="error-container">
+          <h1>Oops!</h1>
+          <Link to="/" className="btn">
+            Back home
+          </Link>
+        </div>
+      </main>
+    </Layout>
+  )
 }
 
 export default Error
